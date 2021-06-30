e12b24d02c3b56e32ff116fbf75ab58ee06ef41e
Jérémie Bannwarth <jban039@aucklanduni.ac.nz>
2021-06-30 14:09:41 +1200

    Add functions to add, subtract, and take the derivative of quaternions

--- attitudeutils/quaternion.py
+++ attitudeutils/quaternion.py
@@ -6,6 +6,119 @@ Written by: Jeremie X. J. Bannwarth
 import numpy as np
 
 
+def quatdiff(q, w):
+    """Calculate the quaternion derivative based on the angular velocity.
+
+    Parameters
+    ----------
+    q : numpy.array
+        Quaternion representing the current orientation.
+    p : numpy.array
+        Current angular velocity vector in rad/s.
+
+    Returns
+    -------
+    qDot : numpy.array
+        Quaternion derivative
+    """
+    B = np.array(
+        [
+            [q[0], -q[1], -q[2], -q[3]],
+            [q[1], q[0], -q[3], q[2]],
+            [q[2], q[3], q[0], -q[1]],
+            [q[3], -q[2], q[1], q[0]],
+        ]
+    )
+
+    qDot = 0.5 * np.dot(B, np.insert(w, 0, 0))
+    return qDot
+
+
+def quatadd(q, p):
+    """Add the rotations defined by two quaternions.
+
+    First perform the rotation of p then the rotation of q.
+    The input order is chosen to match the order of the equations.
+
+    Parameters
+    ----------
+    q : numpy.array
+        Quaternion representing the second rotation.
+    p : numpy.array
+        Quaternion representing the first rotation.
+
+    Returns
+    -------
+    qOut : numpy.array
+        Quaternion representing the total rotation.
+    """
+    qMat = np.array(
+        [
+            [q[0], -q[1], -q[2], -q[3]],
+            [q[1], q[0], q[3], -q[2]],
+            [q[2], -q[3], q[0], q[1]],
+            [q[3], q[2], -q[1], q[0]],
+        ]
+    )
+    r = qMat.dot(p).ravel()
+
+    if r[0] < 0:
+        r = -r
+
+    return r
+
+
+def quatsubtract(r, q, solvefor="first"):
+    """Subtract the rotations defined by two quaternions.
+
+    Solve for q1 or q2 in the rotation r = q2 + q1, where + is the quaternion addition operator.
+    The input order is chosen to match the order of the equations.
+
+    Parameters
+    ----------
+    r : numpy.array
+        Quaternion representing the total rotation.
+    q : numpy.array
+        Quaternion representing the know partial rotation.
+    solvefor : string
+        Which quaternion to solve for. Default: "first" (q1 in r = q2 + q1)
+
+
+    Returns
+    -------
+    qOut : numpy.array
+        Quaternion representing the requested rotation.
+    """
+    if solvefor == "first":
+        # Solve for q1
+        q1Mat = np.array(
+            [
+                [q[0], -q[1], -q[2], -q[3]],
+                [q[1], q[0], q[3], -q[2]],
+                [q[2], -q[3], q[0], q[1]],
+                [q[3], q[2], -q[1], q[0]],
+            ]
+        )
+        qOut = np.dot(q1Mat.T, r)
+    elif solvefor == "second":
+        q2Mat = np.array(
+            [
+                [q[0], -q[1], -q[2], -q[3]],
+                [q[1], q[0], -q[3], q[2]],
+                [q[2], q[3], q[0], -q[1]],
+                [q[3], -q[2], q[1], q[0]],
+            ]
+        )
+        qOut = np.dot(q2Mat.T, r)
+    else:
+        qOut = nan
+
+    if qOut[0] < 0:
+        qOut = -qOut
+
+    return qOut
+
+
 def quat2dcm(q):
     """Convert quaternion to DCM.
 
