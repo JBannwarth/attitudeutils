b6f10c6884493f20bfa74ec4014df6c8de1bcd24
Jérémie Bannwarth <jban039@aucklanduni.ac.nz>
2021-07-04 11:22:01 +1200

    Add Devenport's q-method function

--- attitudeutils/attitude_determination.py
+++ attitudeutils/attitude_determination.py
@@ -3,6 +3,7 @@ Written by: Jeremie X. J. Bannwarth
 """
 
 import numpy as np
+from attitudeutils.quaternion import quat2dcm
 
 
 def triad(v1B, v2B, v1N, v2N):
@@ -53,3 +54,53 @@ def triad(v1B, v2B, v1N, v2N):
     BbarN = np.dot(BbarT, NT.T)
 
     return BbarN
+
+
+def devenportq(vB, vN, w):
+    """ Implementation of Devenport's q-method.
+
+    Inputs are automatically renormalised.
+
+    Parameters
+    ----------
+    vB : numpy.array
+        Nx3 matrix, each row representing an observation vector in body frame.
+    vN : numpy.array
+        Nx3 matrix, each row representing an vector in world frame.
+    w : numpy.array
+        Vector of length N of weights.
+
+    Returns
+    -------
+        Cbar : numpy.array
+        Estimated orientation as a direction cosine matrix.
+    """
+    # Calculate the B matrix
+    N = w.shape[0]
+    B = np.zeros((3, 3))
+    for k in range(0, N):
+        vB[k, :] = vB[k, :] / np.linalg.norm(vB[k, :])
+        vN[k, :] = vN[k, :] / np.linalg.norm(vN[k, :])
+        B = B + w[k] * np.outer(vB[k, :].ravel(), vN[k, :].ravel())
+
+    S = B + B.T
+    sigma = np.trace(B)
+    Z = np.array([[B[1, 2] - B[2, 1]], [B[2, 0] - B[0, 2]], [B[0, 1] - B[1, 0]]])
+
+    # Calculate the K matrix
+    Ktop = np.array([[sigma, Z[0, 0], Z[1, 0], Z[2, 0]]])
+    Kbottom = np.hstack((Z, S - sigma * np.eye(3)))
+    K = np.vstack((Ktop, Kbottom))
+
+    # Find the largest eigenvalue and the corresponding eigenvector/quaternion
+    lambdas, betas = np.linalg.eig(K)
+    idx = np.argmax(lambdas)
+
+    beta = betas[:, idx].ravel()
+
+    if beta[0] < 0:
+        beta = -beta
+
+    Cbar = quat2dcm(beta)
+
+    return Cbar
