ccb1e55fcb7ef1fb463d07496e492118a2e0efa6
Jérémie Bannwarth <jban039@aucklanduni.ac.nz>
2021-07-04 12:47:00 +1200

    Add QUEST algorithm

--- attitudeutils/attitude_determination.py
+++ attitudeutils/attitude_determination.py
@@ -4,6 +4,7 @@ Written by: Jeremie X. J. Bannwarth
 
 import numpy as np
 from attitudeutils.quaternion import quat2dcm
+from scipy import optimize
 
 
 def triad(v1B, v2B, v1N, v2N):
@@ -97,6 +98,62 @@ def devenportq(vB, vN, w):
     idx = np.argmax(lambdas)
 
     beta = betas[:, idx].ravel()
+
+    if beta[0] < 0:
+        beta = -beta
+
+    Cbar = quat2dcm(beta)
+
+    return Cbar
+
+
+def quest(vB, vN, w, tol=1E-10):
+    """ Implementation of the QUEST method.
+
+    Inputs are automatically renormalised.
+    The function does not implement any singularity check.
+
+    Parameters
+    ----------
+    vB : numpy.array
+        Nx3 matrix, each row representing an observation vector in body frame.
+    vN : numpy.array
+        Nx3 matrix, each row representing an vector in world frame.
+    w : numpy.array
+        Vector of length N of weights.
+    tol : double
+        Tolerance for Newton-Raphson algorithm to find the largest eigenvalue.
+
+    Returns
+    -------
+        Cbar : numpy.array
+        Estimated orientation as a direction cosine matrix.
+    """
+    # Calculate the K matrix
+    N = w.shape[0]
+    B = np.zeros((3, 3))
+    for k in range(0, N):
+        vB[k, :] = vB[k, :] / np.linalg.norm(vB[k, :])
+        vN[k, :] = vN[k, :] / np.linalg.norm(vN[k, :])
+        B = B + w[k] * np.outer(vB[k, :].ravel(), vN[k, :].ravel())
+
+    S = B + B.T
+    sigma = np.trace(B)
+    Z = np.array([[B[1, 2] - B[2, 1]], [B[2, 0] - B[0, 2]], [B[0, 1] - B[1, 0]]])
+
+    Ktop = np.array([[sigma, Z[0, 0], Z[1, 0], Z[2, 0]]])
+    Kbottom = np.hstack((Z, S - sigma * np.eye(3)))
+    K = np.vstack((Ktop, Kbottom))
+
+    # Find the largest eigenvalue using the Newton-Raphson iteration method
+    def f(s):
+        return np.linalg.det(K - s*np.eye(4))
+    
+    lambdaOpt = optimize.newton( f, np.sum(w), tol=tol )
+
+    qBar = np.dot( np.linalg.inv((lambdaOpt + sigma)*np.eye(3) - S), Z ).ravel()
+
+    beta = np.insert(qBar,0,1.0) / np.sqrt(1 + np.inner(qBar,qBar))
 
     if beta[0] < 0:
         beta = -beta
